469613ae593f353148d41e3d86c1097d368f8f1f
pwwppw78 <pwwppw78@gmail.com>
2025-01-21 09:46:34 +0100

    Fix Flask app port and host for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,6 @@ def form():
     return render_template("form.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render nutzt PORT-Variable
+    app.run(host="0.0.0.0", port=port)
+
