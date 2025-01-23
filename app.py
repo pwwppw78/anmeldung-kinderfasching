86daef4ec70da937a9446eea3c13149dafaad7de
pwwppw78 <pwwppw78@gmail.com>
2025-01-23 14:51:18 +0100

    Update files

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ def register():
         child_firstname = request.form["child_firstname"]
         child_lastname = request.form["child_lastname"]
         birthdate = request.form["birthdate"]
+        allergies = request.form["allergies"]
+        club_membership = request.form["club-membership"]
         parent_firstname = request.form["parent_firstname"]
         parent_lastname = request.form["parent_lastname"]
         phone_number = request.form["phone_number"]
         email = request.form["email"]
 
-        return render_template("confirmation.html", child_firstname=child_firstname)
+        return render_template("confirmation.html", 
+                                                   child_firstname=child_firstname,
+                                                   child_lastname=child_lastname,
+                                                   birthdate=birthdate)
     
     return render_template("form.html")
 
