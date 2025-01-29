7a7da76fd411cbea722f9617f51905aeb57c4fd0
pwwppw78 <pwwppw78@gmail.com>
2025-01-29 13:01:08 +0100

    Banküberweisung-Button hinzugefügt

--- static/script.js
+++ static/script.js
@@ -1,8 +1,21 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    let isValid = true;
+document.addEventListener("DOMContentLoaded", function() {
+    var bankButton = document.querySelector(".bank-button");
+    var bankDetails = document.getElementById("bank-details");
 
+    if (bankButton) {
+        bankButton.addEventListener("click", function() {
+            if (bankDetails.style.display === "none" || bankDetails.style.display === "") {
+                bankDetails.style.display = "block";
+            } else {
+                bankDetails.style.display = "none";
+            }
+        });
+    }
 
-    if (!isValid) {
-        event.preventDefault();
+    var paypalButton = document.querySelector(".paypal-button");
+    if (paypalButton) {
+        paypalButton.addEventListener("click", function() {
+            window.open("https://paypal.me/pascalweibler?country.x=DE&locale.x=de_DE", "_blank");
+        });
     }
 });
